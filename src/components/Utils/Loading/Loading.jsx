57652ed05511ca8a9f90e42d08b04808646d6625
pietrodev07 <pietro.dev.07@gmail.com>
2023-05-28 09:49:36 +0200

    feat: add loading component

---
+++ src/components/Utils/Loading/Loading.jsx
@@ -0,0 +1,17 @@
+const Loading = () => {
+
+  const loadingStyle = {
+
+    padding: '10px 0'
+
+  }
+
+  return (
+
+    <h2 style={loadingStyle}>Loading...</h2>
+
+  )
+
+}
+
+export default Loading
